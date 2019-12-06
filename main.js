2d588f2c3573d27aafdc6295f650fdafe7b1a081
Robin Fussell <rfussell17@gmail.com>
2019-12-06 14:23:28 -0800

    snake head added

--- main.js
+++ main.js
@@ -6,5 +6,7 @@ window.onload = function(){
   canvas = document.getElementById("gameCanvas");
   canvasContext = canvas.getContext('2d');
 
+  canvasContext.fillStyle = 'yellow';
+  canvasContext.fillRect(100,200,50,50);
 
 }
