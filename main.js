abf195c16a71866015e2199cac312e31c9b17a8b
Robin Fussell <rfussell17@gmail.com>
2019-12-09 10:05:38 -0800

    snake moving by itself

--- main.js
+++ main.js
@@ -1,40 +1,50 @@
 
-let canvas;
-let canvasContext;
-let background;
+const CANVAS = document.getElementById("gameCanvas");
+const context = CANVAS.getContext('2d');
+
+const scale = 25;
+const rows = CANVAS.height / scale;
+const colums = CANVAS.width / scale;
+
 let snakeHeadX = 0;
-let speedX = 10;
 let snakeHeadY = 0;
+
+let speedX = 10;
 let speedY = 0;
+
 let stop = 0;
-let fps = 60;
+let fps = 30;
+
+let background;
 
 
 
 window.onload = function(){
 console.log("onload function")
 
-
-
   setInterval(function(){
+    move();
     draw();
   }, 1000/fps);
-
-  requestAnimationFrame(move);
-
-
-  canvas = document.getElementById("gameCanvas");
-  canvasContext = canvas.getContext('2d');
-
 }
 
-
+function draw(){
+  //background canvas
+  colorHead(0,0,CANVAS.width,CANVAS.height,'green');
+  //head of snake
+  colorHead(snakeHeadX,snakeHeadY,scale,scale,'yellow');
+  //colorBody(bodyX, bodyY, 10, 'red')
+  console.log("draw function")
+}
 
 function move(){
-  console.log("move function")
+  snakeHeadX = snakeHeadX + 3;
+  if(snakeHeadX === 700){
+    snakeHeadX = stop;
+  }
+
+
   function keydown(e) {
-
-
     if(e.keyCode==39){
       snakeHeadX = snakeHeadX + 5;
       console.log("right")
@@ -56,26 +66,14 @@ function move(){
 }
 
 
-
-
-function draw(){
-  //background canvas
-  colorHead(0,0,canvas.width,canvas.height,'green');
-  //head of snake
-  colorHead(snakeHeadX,snakeHeadY,30,30,'yellow');
-  //colorBody(bodyX, bodyY, 10, 'red')
-  console.log("draw function")
-  
-}
-
 function colorBody(centerX, centerY, radius, drawColor){
-  canvasContext.fillStyle = drawColor;
-  canvasContext.beginPath();
-  canvasContext.arc(centerX, centerY, radius, 0,Math.PI*2, true);
-  canvasContext.fill();
+  context.fillStyle = drawColor;
+  context.beginPath();
+  context.arc(centerX, centerY, radius, 0,Math.PI*2, true);
+  context.fill();
 }
 
 function colorHead(leftX,topY, width,height, drawColor){
-  canvasContext.fillStyle = drawColor;
-  canvasContext.fillRect(leftX,topY, width,height)
+  context.fillStyle = drawColor;
+  context.fillRect(leftX,topY, width,height)
 }
