b4cb65c419b12c795d5331738bd45c85c9011535
Robin Fussell <rfussell17@gmail.com>
2019-12-09 11:40:09 -0800

    html table removed

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-
 const CANVAS = document.getElementById("gameCanvas");
-const context = CANVAS.getContext('2d');
+const context = CANVAS.getContext("2d");
 
 const scale = 25;
 const rows = CANVAS.height / scale;
-const colums = CANVAS.width / scale;
+const columns = CANVAS.width / scale;
 
-let snakeHeadX = 0;
+let snakeHeadX = scale * 1;
 let snakeHeadY = 0;
 
-let speedX = 10;
+let speedX = 0;
 let speedY = 0;
 
 let stop = 0;
@@ -17,63 +16,51 @@ let fps = 30;
 
 let background;
 
+window.onload = function() {
+  console.log("onload function");
 
-
-window.onload = function(){
-console.log("onload function")
-
-  setInterval(function(){
+  setInterval(function() {
     move();
     draw();
-  }, 1000/fps);
-}
+  }, 250);
+};
 
-function draw(){
+function draw() {
   //background canvas
-  colorHead(0,0,CANVAS.width,CANVAS.height,'green');
+  colorIn(0, 0, CANVAS.width, CANVAS.height, "green");
   //head of snake
-  colorHead(snakeHeadX,snakeHeadY,scale,scale,'yellow');
-  //colorBody(bodyX, bodyY, 10, 'red')
-  console.log("draw function")
+  colorIn(snakeHeadX, snakeHeadY, scale, scale, "yellow");
 }
 
-function move(){
-  snakeHeadX = snakeHeadX + 3;
-  if(snakeHeadX === 700){
-    snakeHeadX = stop;
-  }
-
-
-  function keydown(e) {
-    if(e.keyCode==39){
-      snakeHeadX = snakeHeadX + 5;
-      console.log("right")
-    }
-    if(e.keyCode==37){
-      snakeHeadX = snakeHeadX - 5;
-      console.log("left")
-    }
-    if(e.keyCode==40){
-      snakeHeadY = snakeHeadY + 5;
-      console.log("down")
-    }
-    if(e.keyCode==38){
-      snakeHeadY = snakeHeadY - 5;
-      console.log("up")
-    }
-  }
-  window.addEventListener("keydown", keydown);
+function colorIn(leftX, topY, width, height, color) {
+  context.fillStyle = color;
+  context.fillRect(leftX, topY, width, height);
 }
 
+function move() {
+  window.addEventListener("keydown", e => {
+    snakeHeadX = speedX;
+    snakeHeadY = speedY;
 
-function colorBody(centerX, centerY, radius, drawColor){
-  context.fillStyle = drawColor;
-  context.beginPath();
-  context.arc(centerX, centerY, radius, 0,Math.PI*2, true);
-  context.fill();
-}
-
-function colorHead(leftX,topY, width,height, drawColor){
-  context.fillStyle = drawColor;
-  context.fillRect(leftX,topY, width,height)
+    if (e.keyCode == 39) {
+      speedX = scale * 1;
+      speedY = 0;
+      console.log("right");
+    }
+    if (e.keyCode == 37) {
+      speedX = -scale * 1;
+      speedY = 0;
+      console.log("left");
+    }
+    if (e.keyCode == 40) {
+      speedX = 0;
+      speedY = scale * 1;
+      console.log("down");
+    }
+    if (e.keyCode == 38) {
+      speedX = 0;
+      speedY = -scale * 1;
+      console.log("up");
+    }
+  });
 }
